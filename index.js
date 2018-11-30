74f3f3313d45c34f25a0bb270b4d253dcfd28f11
Joanna Tomassoni <jctomassoni@gmail.com>
2018-11-30 03:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = {};
+  var counter = 0;
+  for (counter = 0; counter < 5; counter++) {
+    beatles.push(`${musicians} + " plays " + ${instruments}`);
+  }
+  return beatles;
+}
